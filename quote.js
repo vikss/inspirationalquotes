15583abee44d2daf5f76df9c5d711abc642e8340
vikss <viraktijain01@gmail.com>
2022-10-03 21:13:37 +0530

    Modified the code a bit

--- quote.js
+++ quote.js
@@ -1,32 +1,34 @@
 let quoteElem = document.getElementById("quoteId");
 let authorElem = document.getElementById("authorId");
 
+
 async function getQuote() {
 
-    let result = await fetch("https://api.goprogram.ai/inspiration")
-    let data = await result.json();
-
-    try{
-    if (data) {
-        console.log(data)
-        let textNode = document.createTextNode(data.quote)
-        quoteElem.append(textNode)
-        let authorName = document.createTextNode("-" + data.author)
-        authorElem.append(authorName)
-    }
-}
-    catch (error) {
+    let result = await fetch("https://api.goprogram.ai/inspiration").catch(function (err) {
         chrome.storage.sync.get(['quote', "author"], (data) => {
             console.log(data.quote)
             console.log(data.author)
             let textNode = document.createTextNode(data.quote)
+            let authorName = document.createTextNode("-" + data.author)
             quoteElem.append(textNode)
-            let authorName = document.createTextNode("- " + data.author)
             authorElem.append(authorName)
 
 
         });
+    })
+    let data = await result.json();
+
+
+    if (data) {
+        console.log(data)
+        let textNode = document.createTextNode(data.quote)
+        let authorName = document.createTextNode("-" + data.author)
+
+        quoteElem.append(textNode)
+        authorElem.append(authorName)
     }
+
+
 
 }
 
